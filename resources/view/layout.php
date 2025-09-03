a12d421d1ac16736d02cf6e4ab67593e9910fcae
Juan Millan <juanes14569@gmail.com>
2025-09-03 11:58:11 +0200

    update layout metas

--- resources/view/layout.php
+++ resources/view/layout.php
@@ -5,7 +5,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pesto</title>
+    <title>Pesto - Expressive Templates</title>
+
+    <!-- SEO -->
+    <meta name="description" content="Modern PHP template engine that provides an intuitive and expressive way to build web application views. It offers a clean syntax using custom HTML attributes and supports advanced templating features like view composition, slots, conditional rendering, loops, and built-in security measures.">
+    <meta name="keywords" content="Pesto, PHP, Template Engine, Templates, Views">
+    <meta name="author" content="Juan Millan">
+    <link rel="canonical" href="https://pestophp.com/">
+
+    <!-- Social -->
+    <meta property="og:title" content="Pesto - Expressive Templates">
+    <meta property="og:description" content="Modern PHP template engine that provides an intuitive and expressive way to build web application views. It offers a clean syntax using custom HTML attributes and supports advanced templating features like view composition, slots, conditional rendering, loops, and built-in security measures.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://pestophp.com/">
+    <meta property="og:image" content="https://pestophp.com/img/template.html.png">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Pesto - Expressive Templates">
+    <meta name="twitter:description" content="Modern PHP template engine that provides an intuitive and expressive way to build web application views. It offers a clean syntax using custom HTML attributes and supports advanced templating features like view composition, slots, conditional rendering, loops, and built-in security measures.">
+    <meta name="twitter:image" content="https://pestophp.com/img/template.html.png">
+
 
     <script php-if="$vite->isRunningDevServer()"
             type="module"
